2d949cb8b9519cd34f180353adb0e2f30e1d5682
Jeremy Felt <jeremy.felt@gmail.com>
2017-10-11 16:55:59 -0700

    Update initial plugin structure

--- plugin.php
+++ plugin.php
@@ -8,17 +8,16 @@ Author URI: https://web.wsu.edu/
 Plugin URI: https://github.com/washingtonstateuniversity/WSUWP-Extended-WP-Event-Calendar
 */
 
-namespace WSU\WPEventCalendar;
-
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-add_action( 'plugins_loaded', 'WSU\WPEventCalendar\bootstrap' );
-/**
- * Loads the rest of the WSUWP Extended WP Event Calendar.
- *
- * @since 0.0.1
- */
-function bootstrap() {}
+// This plugin uses namespaces and requires PHP 5.3 or greater.
+if ( version_compare( PHP_VERSION, '5.3', '<' ) ) {
+	add_action( 'admin_notices', create_function( '',
+	"echo '<div class=\"error\"><p>" . __( 'WSUWP Extended WP Event Calendar requires PHP 5.3 to function properly. Please upgrade PHP or deactivate the plugin.', 'wsuwp-extended-wp-event-calendar' ) . "</p></div>';" ) );
+	return;
+} else {
+	include_once __DIR__ . '/includes/events.php';
+}
