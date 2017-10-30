7d1e38cab6c9409b5e58d894b49a4d2dddd2c135
Phil Cable <pcable@wsu.edu>
2017-10-30 10:28:30 -0700

    Register University taxonomies for the `Events` post type

--- includes/taxonomies.php
+++ includes/taxonomies.php
@@ -3,6 +3,7 @@
 namespace WSU\Events\Taxonomies;
 
 add_action( 'init', 'WSU\Events\Taxonomies\unregister_types', 11 );
+add_action( 'init', 'WSU\Events\Taxonomies\register_university_taxonomies', 12 );
 
 /**
  * Removes the Types taxonomy from the Events post type.
@@ -11,4 +12,15 @@ add_action( 'init', 'WSU\Events\Taxonomies\unregister_types', 11 );
  */
 function unregister_types() {
 	unregister_taxonomy_for_object_type( 'event-type', 'event' );
+}
+
+/**
+ * Registers University Taxonomies for the Events post type.
+ *
+ * @since 0.0.1
+ */
+function register_university_taxonomies() {
+	register_taxonomy_for_object_type( 'wsuwp_university_category', 'event' );
+	register_taxonomy_for_object_type( 'wsuwp_university_location', 'event' );
+	register_taxonomy_for_object_type( 'wsuwp_university_org', 'event' );
 }
