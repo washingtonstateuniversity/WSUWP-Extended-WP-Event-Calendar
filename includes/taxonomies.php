2577786bd16af375e171cdb723e6bef7f607b935
Phil Cable <pcable@wsu.edu>
2017-10-30 10:29:17 -0700

    Use the Select2 interface for taxonomy term selection

--- includes/taxonomies.php
+++ includes/taxonomies.php
@@ -4,6 +4,7 @@ namespace WSU\Events\Taxonomies;
 
 add_action( 'init', 'WSU\Events\Taxonomies\unregister_types', 11 );
 add_action( 'init', 'WSU\Events\Taxonomies\register_university_taxonomies', 12 );
+add_filter( 'wsuwp_taxonomy_metabox_post_types', 'WSU\Events\Taxonomies\taxonomy_meta_box', 10 );
 
 /**
  * Removes the Types taxonomy from the Events post type.
@@ -23,4 +24,17 @@ function register_university_taxonomies() {
 	register_taxonomy_for_object_type( 'wsuwp_university_category', 'event' );
 	register_taxonomy_for_object_type( 'wsuwp_university_location', 'event' );
 	register_taxonomy_for_object_type( 'wsuwp_university_org', 'event' );
+}
+
+/**
+ * Displays a meta box for selecting taxonomy terms.
+ *
+ * @since 0.0.1
+ *
+ * @param array $post_types Post types and their associated taxonomies.
+ */
+function taxonomy_meta_box( $post_types ) {
+	$post_types['event'] = get_object_taxonomies( 'event' );
+
+	return $post_types;
 }
