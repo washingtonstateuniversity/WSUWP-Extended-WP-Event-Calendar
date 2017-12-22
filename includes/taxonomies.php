2b4de54b028a4680dbba6ad81560ac2eb70affa6
Phil Cable <pcable@wsu.edu>
2017-12-22 09:20:11 -0800

    Reenable the Event Types taxonomy

--- includes/taxonomies.php
+++ includes/taxonomies.php
@@ -2,18 +2,8 @@
 
 namespace WSU\Events\Taxonomies;
 
-add_action( 'init', 'WSU\Events\Taxonomies\unregister_types', 11 );
 add_action( 'init', 'WSU\Events\Taxonomies\register_university_taxonomies', 12 );
 add_filter( 'wsuwp_taxonomy_metabox_post_types', 'WSU\Events\Taxonomies\taxonomy_meta_box', 10 );
-
-/**
- * Removes the Types taxonomy from the Events post type.
- *
- * @since 0.0.1
- */
-function unregister_types() {
-	unregister_taxonomy_for_object_type( 'event-type', 'event' );
-}
 
 /**
  * Registers University Taxonomies for the Events post type.
