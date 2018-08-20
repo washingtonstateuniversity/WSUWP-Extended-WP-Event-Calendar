b2b39dd80c6c14480080741f64d54e001e5e1527
Phil Cable <pcable@wsu.edu>
2018-08-20 15:36:47 -0700

    Bump version 0.1.11

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WSUWP Extended WP Event Calendar
-Version: 0.1.10
+Version: 0.1.11
 Description: WSU extensions of WP Event Calendar
 Author: washingtonstateuniversity, jeremyfelt
 Author URI: https://web.wsu.edu/
